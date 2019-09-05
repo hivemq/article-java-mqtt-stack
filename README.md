3d05ff8f28836215c9627700a2d87e3a2f02764f
Anja Helmbrecht-Schaar <anja.helmbrecht-schaar@dc-square.de>
2019-09-05 16:22:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# article-java-mqtt-stack
-Sample code for building a full stack MQTT solution with open source components
+# Article-java-mqtt-stack
+This repository includes all information to run the described sample from the article 
+for building a full stack MQTT solution with java based open source components.
+
+## Steps to run the full Example 
+
+### Prerequisites
+* Java 11 
+* Maven  
+
+
+### Message Broker
+* Download the latest HiveMQ archive from [HiveMQ - Developers getting started](https://www.hivemq.com/developers/getting-started/)
+* Unpack
+* Run start HiveMQ
+
+### Backend MQTT Client
+* Compile and build java-mqtt-client example from the module
+* Details are described in [java-mqtt-client/README.md](java-mqtt-client/README.md)
+* Start Main class from java-mqtt-client via IDE or as
+`java -jar java-mqtt-client/target/backend-client-1.0-SNAPSHOT.jar `
+
+### MQTT Client Interface
+* Get a package of your choice from HiveMQ-CLI via [HiveMQ-CLI github](https://github.com/hivemq/hivemq-cli)
+* Install hivemq-cli or build the jar (gradle build)
+* Run the script `example-devices.sh ` 
+
+
+### Further Documentation
+* https://github.com/hivemq/hivemq-community-edition
+* https://hivemq.github.io/hivemq-mqtt-client/
+* https://hivemq.github.io/hivemq-cli/
+* https://www.hivemq.com/  
+* https://www.hivemq.com/docs/4.2/hivemq/introduction.html
+* https://www.hivemq.com/mqtt-essentials/  
+* https://www.hivemq.com/extensions/
---
+++ java-mqtt-client/src/main/java/com/hivemq/mqttclient/example/climate/ClimateBackend.java
@@ -0,0 +1,127 @@
+package com.hivemq.mqttclient.example.climate;
+
+import com.hivemq.client.mqtt.MqttClient;
+import com.hivemq.client.mqtt.datatypes.MqttQos;
+import com.hivemq.client.mqtt.datatypes.MqttTopicFilter;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
+import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
+import com.hivemq.client.mqtt.mqtt5.message.subscribe.Mqtt5Subscription;
+import org.pmw.tinylog.Logger;
+
+import java.nio.ByteBuffer;
+
+import static com.hivemq.mqttclient.example.common.Utils.*;
+
+public class ClimateBackend {
+
+    private static final String CLIMATE_CONTROLLER_BACKEND = "ClimateControllerBackend";
+    private static final String TOPIC_ROOMS_TEMP = "room/+/temperature";
+    private static final String DECREASE = "DOWN";
+    private static final String INCREASE = "UP";
+
+    private static String temperatureControl(Integer val) {
+        return (val < 15) ? INCREASE : (val > 30) ? DECREASE : null;
+    }
+
+    public void startClimateControllerBackend() {
+        final Mqtt5AsyncClient client = MqttClient.builder()
+                .serverHost(BROKER_HIVEMQ_ADR)
+                .serverPort(BROKER_HIVEMQ_PORT)
+                .useMqttVersion5()
+                .identifier(CLIMATE_CONTROLLER_BACKEND)
+                .buildAsync();
+
+        client.connectWith()
+                .keepAlive(KEEP_ALIVE)
+                .send()
+                .whenComplete((connAck, throwable) -> {
+                    if (throwable != null) {
+                        Logger.error("Connect client {} failed with reason: {}  ",
+                                client.getConfig().getClientIdentifier().get(), throwable.getMessage());
+                    } else {
+                        Logger.info("Connect client {} " , client.getConfig().getClientIdentifier().get());
+                        doSubscribeToRooms(client);
+                    }
+                });
+
+        addDisconnectOnRuntimeShutDownHock(client);
+    }
+
+    private void doSubscribeToRooms(Mqtt5AsyncClient client) {
+
+        Logger.info("Start subscribing to rooms temperature topic");
+
+        final Mqtt5Subscription subscriptionTemperature = Mqtt5Subscription.builder()
+                .topicFilter(TOPIC_ROOMS_TEMP)
+                .qos(MqttQos.AT_LEAST_ONCE).build();
+
+        client.subscribeWith()
+                .addSubscription(subscriptionTemperature)
+                .callback(publish -> {
+                    Logger.info("Message received on topic: {} with payload: {} and with QoS: {}" ,publish.getTopic(), new String(publish.getPayloadAsBytes()), publish.getQos().getCode());
+                    doPublishCommand(client, publish);
+                })
+                .send()
+                .whenComplete((subAck, throwable) -> {
+                    if (throwable != null) {
+                        Logger.error("Subscription to topics failed. " , throwable.getMessage());
+                    } else {
+                        Logger.info("Successful subscribed to topics: {} ", TOPIC_ROOMS_TEMP);
+                    }
+                });
+    }
+
+    private void doPublishCommand(Mqtt5AsyncClient client, Mqtt5Publish publish) {
+
+        final Integer val = getIntValueFromPayload(publish);
+        Logger.info("Try publish to topic: {}/command ", publish.getTopic() );
+
+        if (MqttTopicFilter.of(TOPIC_ROOMS_TEMP).matches(publish.getTopic())) {
+            if (val != null) {
+                final String result = temperatureControl(val);
+                if( result != null) {
+                    publishCommand(client, result, publish.getTopic().toString());
+                } else {
+                    Logger.info("Temperature ok");
+                }
+            }
+        } else {
+            Logger.warn("Topic not match to command filter: {} ", publish.getTopic().toString());
+        }
+    }
+
+    private Integer getIntValueFromPayload(Mqtt5Publish publish) {
+        ByteBuffer buffer = (publish.getPayload().isPresent()) ? publish.getPayload().get() : null;
+        if (buffer != null) {
+            byte[] dst = new byte[buffer.limit()];
+            try {
+                // deep copy necessary
+                buffer.get(dst);
+                return Integer.valueOf(new String(dst));
+            } catch (Exception any) {
+                Logger.error("Cannot read integer value from Payload, error: ", any.getMessage());
+            }
+        }
+        return null;
+    }
+
+    private void publishCommand(Mqtt5AsyncClient client, String payload, String topic) {
+        if (payload != null) {
+            final Mqtt5Publish command = Mqtt5Publish.builder()
+                    .topic(topic + "/command")
+                    .qos(MqttQos.AT_LEAST_ONCE)
+                    .contentType("TEXT")
+                    .payload(payload.getBytes())
+                    .build();
+
+            client.publish(command).whenComplete((publishResult, throwable) -> {
+                if (throwable != null) {
+                    Logger.error("Publish to topic: {} failed, ", topic , throwable.getMessage());
+                } else {
+                    Logger.info("Publish msg '{}' to topic: {}/command" ,payload, topic);
+                }
+            });
+        }
+    }
+
+}
